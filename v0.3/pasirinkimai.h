283d01edfd657829b1a5365467eb92579167fd15
GintareeJ <gjonaityte1@gmail.com>
2026-03-11 14:25:12 +0200

    Add new functions for file generation in pasirinkimai.h

--- v0.3/pasirinkimai.h
+++ v0.3/pasirinkimai.h
@@ -16,5 +16,7 @@ void PirmasP(std::vector<studentas>& studentai, int& b);
 void AntrasP(std::vector<studentas>& studentai, int& b);
 void TreciasP(std::vector<studentas>& studentai, const std::vector<std::string>& pavardes,  const std::vector<std::string>& vardai, int& b);
 void KetvirtasP(std::vector<studentas>& studentai, const std::string& CVfd, const std::string& CVfr, double& suma2, double& suma3, double& suma4, double& suma5, double& suma6, int& spausd, bool& skaitytaIsFailo, int& b);
+void GeneruotiStudentuFaila(const std::string& failoPav, int studentuKiekis, int ndKiekis);
+void GeneruotiVisusFailus();
 
 #endif
