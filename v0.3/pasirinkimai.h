699e120171f10d23748448c1aac7777bf3f280d9
GintareeJ <gjonaityte1@gmail.com>
2026-03-11 17:44:24 +0200

    Modify function signatures in pasirinkimai.h
    Updated function signatures and added parameters for NuskaitytiIsFailo.

--- v0.3/pasirinkimai.h
+++ v0.3/pasirinkimai.h
@@ -5,7 +5,7 @@
 #include "studentas.h"
 #include "rusiavimas.h"
 
-void Skaiciavimai(std::vector<studentas>& studentai);
+void Skaiciavimai(std::vector<studentas>& studentai, int b);
 void Spausdinimas(std::vector<studentas>& studentai, int& b);
 void VardasPavarde(std::vector<studentas>& studentai, studentas& s);
 int kinta();
@@ -17,7 +17,7 @@ void AntrasP(std::vector<studentas>& studentai, int& b);
 void TreciasP(std::vector<studentas>& studentai, const std::vector<std::string>& pavardes,  const std::vector<std::string>& vardai, int& b);
 void KetvirtasP(std::vector<studentas>& studentai, const std::string& CVfd, const std::string& CVfr, double& suma2, double& suma3, double& suma4, double& suma5, double& suma6, int& spausd, bool& skaitytaIsFailo, int& b);
 void GeneruotiStudentuFaila(const std::string& failoPav, int studentuKiekis, int ndKiekis);
-void NuskaitytiIsFailo(std::vector<studentas>& studentai, const std::string& failoPav);
+void NuskaitytiIsFailo(std::vector<studentas>& studentai, const std::string& failoPav, int studentuKiekis);
 void PadalintiStudentus(const std::vector<studentas>& studentai, std::vector<studentas>& vargsiukai, std::vector<studentas>& kietiakiai, int b);
 void RusiuotiStudentus(std::vector<studentas>& studentai, int b, int r, int rus);
 template <typename Konteineris>
