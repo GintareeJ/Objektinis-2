9d2a96f6657a3876852a3aa9a199fca3fd929304
GintareeJ <gjonaityte1@gmail.com>
2026-03-18 13:25:04 +0200

    Update pasirinkimai.h

--- v0.4/pasirinkimai.h
+++ v0.4/pasirinkimai.h
@@ -17,6 +17,7 @@ void AntrasP(std::vector<studentas>& studentai, int& b);
 void TreciasP(std::vector<studentas>& studentai, const std::vector<std::string>& pavardes,  const std::vector<std::string>& vardai, int& b);
 void KetvirtasP(std::vector<studentas>& studentai, const std::string& CVfd, const std::string& CVfr, double& suma2, double& suma3, double& suma4, double& suma5, double& suma6, int& spausd, bool& skaitytaIsFailo, int& b);
 void GeneruotiStudentuFaila(const std::string& failoPav, int studentuKiekis, int ndKiekis);
+void Tyrimas1(const std::string& failoPav, int studentuKiekis, int ndKiekis);
 void NuskaitytiIsFailo(std::vector<studentas>& studentai, const std::string& failoPav, int studentuKiekis);
 void PadalintiStudentus(const std::vector<studentas>& studentai, std::vector<studentas>& vargsiukai, std::vector<studentas>& kietiakiai, int b);
 void RusiuotiStudentus(std::vector<studentas>& studentai, int b, int r, int rus);
