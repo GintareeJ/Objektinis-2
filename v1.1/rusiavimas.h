9b0a3849081d1e6e2922270c272fd3af902421e1
GintareeJ <gjonaityte1@gmail.com>
2026-04-02 18:56:37 +0300

    Update rusiavimas.h

--- v1.1/rusiavimas.h
+++ v1.1/rusiavimas.h
@@ -2,16 +2,15 @@
 #define RUSIAVIMAS_H
 
 #include "studentas.h"
-#include <vector>
 
-bool DidPav(studentas &A, studentas &B);
-bool DidVar(studentas &A, studentas &B);
-bool DidVid(studentas &A, studentas &B);
-bool DidMed(studentas &A, studentas &B);
+bool DidPav(const Studentas &A, const Studentas &B);
+bool DidVar(const Studentas &A, const Studentas &B);
+bool DidVid(const Studentas &A, const Studentas &B);
+bool DidMed(const Studentas &A, const Studentas &B);
 
-bool MazPav(studentas &A, studentas &B);
-bool MazVar(studentas &A, studentas &B);
-bool MazVid(studentas &A, studentas &B);
-bool MazMed(studentas &A, studentas &B);
+bool MazPav(const Studentas &A, const Studentas &B);
+bool MazVar(const Studentas &A, const Studentas &B);
+bool MazVid(const Studentas &A, const Studentas &B);
+bool MazMed(const Studentas &A, const Studentas &B);
 
 #endif
